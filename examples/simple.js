f73d121ee13ff9fb60b2fd0f816ed2bb93e0c20a
Dario M. Mannu <d.mannu@cubelets.digital>
2016-12-20 17:05:50 +0000

    add more examples

--- examples/simple.js
+++ examples/simple.js
@@ -6,6 +6,21 @@ console.log(babar([[0, 1], [1, 5], [2, 5], [3, 1], [4, 6]], {
   color: 'green',
   width: 40,
   height: 10,
-  color: 'green',
   yFractions: 1
+}));
+
+console.log(babar([
+	[0, 9],
+	[1, 20],
+	[2, 33],
+	[3, 20],
+	[4, 12],
+	[5, 10],
+	[6, 63],
+	[7, 21]
+], {
+  width: 80,
+  height: 10,
+  color: 'yellow',
+  maxY: 100
 }));
